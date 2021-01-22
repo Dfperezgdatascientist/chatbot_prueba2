2eb9b7486f4a35aea063e35c2c70629ff1f1814f
Diego Mauricio Mejía Ospina <77675522+DiegoMejiaO@users.noreply.github.com>
2021-01-22 10:00:02 -0500

    Update app.py
    Revision 3.0

--- app.py
+++ app.py
@@ -122,46 +122,28 @@ def webhook():
                                 #Update states
                                 states_value = [hidden_state, cell_state]      
                             return decoded_sentence
-
-                        class ChatBot:
-                            negative_responses = ("no", "nope", "nah", "naw", "no gracias", "lo siento")
-                            #--exit_commands = ("cerrar", "salir", "partir", "chao", "hasta luego", "parar", "detener", "terminar")
-                            #Method to start the conversation
-                            def start_chat(self):
-                                user_response = input("Hola, soy un chatbot entrenado en diálogos de politica. ¿Te gustaría chatear conmigo?\n")
-                                
-                                if user_response in self.negative_responses:
-                                #--print("Ok, Que tengas un gran día!")
-                                    send_message(sender_id, "Ok, Que tengas un gran día!")
-                                    return
-                                self.chat(user_response)
-                            #Method to handle the conversation
-                            def chat(self, reply):
-                                while not self.make_exit(reply):
-                                    reply = input(self.generate_response(reply)+"\n")
-                                
-                            #Method to convert user input into a matrix
-                            def string_to_matrix(self, user_input):
-                                tokens = re.findall(r"[\w']+|[^\s\w]", user_input)
-                                user_input_matrix = np.zeros(
-                                    (1, max_encoder_seq_length, num_encoder_tokens),
-                                    dtype='float32')
-                                for timestep, token in enumerate(tokens):
-                                    if token in input_features_dict:
-                                        user_input_matrix[0, timestep, input_features_dict[token]] = 1.
-                                return user_input_matrix
+                                                     
+                        #Method to convert user input into a matrix
+                        def string_to_matrix(user_input):
+                            tokens = re.findall(r"[\w']+|[^\s\w]", user_input)
+                            user_input_matrix = np.zeros(
+                                (1, max_encoder_seq_length, num_encoder_tokens),
+                                dtype='float32')
+                            for timestep, token in enumerate(tokens):
+                                if token in input_features_dict:
+                                    user_input_matrix[0, timestep, input_features_dict[token]] = 1.
+                            return user_input_matrix
+                        
+                        #Method that will create a response using seq2seq model we built
+                        def generate_response(user_input):
+                            input_matrix = string_to_matrix(user_input)    
+                            chatbot_response = decode_response(input_matrix)    
+                            #Remove <START> and <END> tokens from chatbot_response
+                            chatbot_response = chatbot_response.replace("<START>",'')
+                            chatbot_response = chatbot_response.replace("<END>",'')
+                            return chatbot_response                                   
                             
-                            #Method that will create a response using seq2seq model we built
-                            def generate_response(self, user_input):
-                                input_matrix = self.string_to_matrix(user_input)    
-                                chatbot_response = decode_response(input_matrix)    
-                                #Remove <START> and <END> tokens from chatbot_response
-                                chatbot_response = chatbot_response.replace("<START>",'')
-                                chatbot_response = chatbot_response.replace("<END>",'')
-                                #--return chatbot_response
-                                send_message(sender_id, chatbot_response)   
-                            
-                        #--send_message(sender_id, chatbot_response)
+                        send_message(sender_id, generate_response(test_input))
                     else:
                         send_message(sender_id, 'Hola')
 
